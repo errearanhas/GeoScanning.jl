54a8b0873f12abf03e037b14bf307a461d9252b6
Renato Aranha <errearanhas@gmail.com>
2020-03-18 11:50:22 -0300

    add GeoStatsBase and ISC disclaimer

--- src/GeoScanning.jl
+++ src/GeoScanning.jl
@@ -1,5 +1,21 @@
+# ------------------------------------------------------------------
+# Licensed under the ISC License. See LICENSE in the project root.
+# ------------------------------------------------------------------
+
 module GeoScanning
 
-greet() = print("Hello World!")
+import GeoStatsBase
+export GeoSCAN
+
+"""
+GeoSCAN(eps, minpts)
+"""
+struct GeoSCAN{T<:Real} <: AbstractLearningSolver
+  eps::T
+  minpts::Int
+end
+
+function solve(problem::LearningProblem, solver::GeoSCAN)
+end
 
 end # module
